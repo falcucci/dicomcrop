76aeeef81b42b72fd3ed64cdd4e5b9c80ee0fa28
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-01-29 01:47:01 +0100

    adds support for fire cli to the `dicomcrop/cli.py` file. this allows for easier command line usage of the `crop_images` and `crop` functions, as well as the `edges` function.
    the changes made to the `dicomcrop/cli.py` file include adding the `fire.fire` function with the `--dir`, `--image`, and `--edges` arguments. this allows for easier command line usage of the functions.

--- dicomcrop/cli.py
+++ dicomcrop/cli.py
@@ -69,4 +69,8 @@ def crop_images(directory, output=''):
 
 if __name__ == '__main__':
     import fire
-    fire.Fire({'--dir': crop_images, '--image': crop, '--edges': edges})
+    fire.Fire({
+        '--dir': crop_images,
+        '--image': crop,
+        '--edges': edges
+    })
