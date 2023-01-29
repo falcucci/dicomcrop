760223318b09ecfc474a5ea630b8c5c6fbc1066e
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-01-29 16:33:30 +0100

    adds a `setup.py` file to the project. this file is used to define the project's metadata, such as its name, version, description, author, license, and dependencies.
    the `setup.py` file contains the following information:
    - name: `dicomcrop`
    - version: `__version__`
    - description: `prepare bedside medical images for machine learning and image interpretation.`
    - long description: `readme.md`
    - url: `https://github.com/falcucci/dicomcrop`
    - author: `alexsander falcucci`
    - author email: `alex.falcucci@gmail.com`
    - license: `apache software license 2.0`
    - classifiers: `intended audience :: healthcare industry`, `topic :: scientific/engineering :: medical science apps.`, `programming language :: python :: 3.7`, `programming language :: python :: 3.8`
    - keywords: `dicom bedside pocus ffmpeg opencv echo cardiac cancer hospital`
    - packages: `find_packages()`
    - python requires: `>=3.6`
    - install requires: `cffi`, `cryptography`, `dicom2jpg`, `et-xmlfile`, `fire`, `numpy`, `opencv-python`, `openpyxl`, `pillow`, `pycparser`, `pydicom`, `pyjwt`, `pylibjpeg`, `pylibjpeg-libjpeg`, `pylibjpeg-openjpeg`, `six`, `termcolor`
    
    this commit adds the `setup.py` file to the project, which is necessary for the project to be properly packaged and distributed.

---
+++ setup.py
@@ -0,0 +1,68 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = {}
+with open(path.join(here, "dicomcrop", "__version__.py")) as f:
+    exec(f.read(), version)
+
+setup(
+
+  name='dicomcrop',
+
+  version=version["__version__"],
+
+  description='Prepare bedside medical images for machine learning and image interpretation.',
+
+  long_description=long_description,
+
+  long_description_content_type='text/markdown',
+
+  url='https://github.com/falcucci/dicomcrop',
+
+  author='Alexsander Falcucci',
+
+  author_email='alex.falcucci@gmail.com',
+
+  license = "Apache Software License 2.0",
+
+
+  classifiers=[
+    'Intended Audience :: Healthcare Industry',
+    'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+
+  keywords='dicom bedside pocus ffmpeg opencv echo cardiac cancer hospital',
+
+  packages=find_packages(),
+
+  python_requires='>=3.6',
+
+  install_requires=[
+      'cffi',
+      'cryptography',
+      'dicom2jpg',
+      'et-xmlfile',
+      'fire',
+      'numpy',
+      'opencv-python',
+      'openpyxl',
+      'Pillow',
+      'pycparser',
+      'pydicom',
+      'PyJWT',
+      'pylibjpeg',
+      'pylibjpeg-libjpeg',
+      'pylibjpeg-openjpeg',
+      'six',
+      'termcolor'
+  ],
+
+)
